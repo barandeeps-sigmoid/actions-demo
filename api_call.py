44787dcc1f770cfa6ef29a92965cf0dc3234af34
Baran <barandeeps@sigmoidanalytics.com>
2022-01-17 18:27:04 +0530

    test

--- api_call.py
+++ api_call.py
@@ -1,41 +1,36 @@
-import requests
 import os
+import requests
 import subprocess
 import sys
 
 URL = "https://api.github.com/repos/barandeeps-sigmoid/actions-demo/pulls?state=closed"
-PARAMS= {"base": sys.argv[1]}
+PARAMS = {"base": sys.argv[1]}
 
 
 def get_closed_pr_list():
     data = requests.get(url=URL, params=PARAMS)
+    print(data.json())
     return data.json()
 
 
 def main():
-    closed_pr_list = get_closed_pr_list() 
+    try:
+        closed_pr_list = get_closed_pr_list()
 
-    num = len(closed_pr_list)
+        for item in range(0, 2):
+            print(closed_pr_list[item]["url"])
+            print(closed_pr_list[item]["number"])
+            print(closed_pr_list[item]["merged_at"])
+            print(closed_pr_list[item]["merge_commit_sha"])
+            print("=======================")
 
-
-    for item in range(0, 2):
-        print(closed_pr_list[item]["url"])
-        print(closed_pr_list[item]["number"])
-        print(closed_pr_list[item]["merged_at"])
-        print(closed_pr_list[item]["merge_commit_sha"])
-        print("=======================")
-
-
-
-    print()
-
-    print(f"{closed_pr_list[1]["merge_commit_sha"]},{closed_pr_list[0]["merge_commit_sha"]}")
-
+        print()
+        print(f"""{closed_pr_list[1]["merge_commit_sha"]},{closed_pr_list[0]["merge_commit_sha"]}""")
+    except Exception as ex:
+        print("Caught Exception")
+        print(ex)
 
 
 if __name__ == "__main__":
+    print("Hello")
     main()
-
-
-
-
