d24032785e0868676165bc8f83b6f01c32979b9e
Baran <barandeeps@sigmoidanalytics.com>
2022-01-19 19:09:43 +0530

    test

--- copy_changed_files_to_temp_folder.sh
+++ copy_changed_files_to_temp_folder.sh
@@ -19,13 +19,14 @@ mkdir -p $TEMP_DIR_PATH
 my_array=($(echo $RESULT | tr ";" "\n"))
 for file_name in "${my_array[@]}";do
    echo "::set-output name=VALIDATE_CHANGES_SCHEMACHANGE::TRUE"
-   EPOCH_TIMESTAMP_WITH_MILLIS=".$(date +%s%3N)_"
+   EPOCH_TIMESTAMP_WITH_MILLIS=".$(date +%s.%3N)_"
    echo "Original file name with path $file_name"
    EXTRACTED_FILE_NAME=$(echo "$file_name" | sed "s/.*\///")
    echo "Extracted file name only $EXTRACTED_FILE_NAME"
    REPLACED_FILE_NAME=${EXTRACTED_FILE_NAME/_/$EPOCH_TIMESTAMP_WITH_MILLIS}
    echo "Replaced file name $REPLACED_FILE_NAME"
    cp "$file_name" "$TEMP_DIR_PATH/$REPLACED_FILE_NAME"
+   sleep 0.0001
    echo "============================================="
 done
 
